869fd01fc393a9c22f9c89f057f94f73d2c804d3
915_Nagy_Matyas <tomesjerry3@gmail.com>
2022-09-12 20:52:31 +0300

    added tag crafting
    yea minecraft:shulker_boxes is found in fodler '../blocks', yet its actually an item

--- create_pack.py
+++ create_pack.py
@@ -7,9 +7,15 @@ os.mkdir('./temp', mode=0o777)
 os.mkdir('./temp/data', mode=0o777)
 os.mkdir('./temp/data/crafting', mode=0o777)
 os.mkdir('./temp/data/crafting/recipes', mode=0o777)
+os.mkdir('./temp/data/grouping', mode=0o777)
+os.mkdir('./temp/data/grouping/items', mode=0o777)
 
 with open('./temp/pack.mcmeta', 'w') as meta:
-    meta.write('{"pack":{"pack_format":10,"description":"\u00A75\u00A7l\u00A7oShulker Boxed Mass Crafting\u00A7r by \u00A7bu/Tom3s\u00A7r \\n\u00A7c\u00A7lNBT Crafting\u00A7r mod by \u00A7bu/Siphalor \u00A7rrequired!!"}}')
+    meta.write('{"pack":{"pack_format":10,"description":"Shulker Boxed Mass Crafting by u/Tom3s \\nNBT Crafting mod by u/Siphalor required!!"}}')
+
+with open('./temp/data/grouping/items/shulkers.json', 'w') as shulkers:
+    shulkers.write('{\n\t"values": [\n\t\t"minecraft:shulker_box",\n\t\t"minecraft:black_shulker_box",\n\t\t"minecraft:blue_shulker_box",\n\t\t"minecraft:brown_shulker_box",\n\t\t"minecraft:cyan_shulker_box",\n\t\t"minecraft:gray_shulker_box",\n\t\t"minecraft:green_shulker_box",\n\t\t"minecraft:light_blue_shulker_box",\n\t\t"minecraft:light_gray_shulker_box",\n\t\t"minecraft:lime_shulker_box",\n\t\t"minecraft:magenta_shulker_box",\n\t\t"minecraft:orange_shulker_box",\n\t\t"minecraft:pink_shulker_box",\n\t\t"minecraft:purple_shulker_box",\n\t\t"minecraft:red_shulker_box",\n\t\t"minecraft:white_shulker_box",\n\t\t"minecraft:yellow_shulker_box"\n\t]\n}')
+
 
 
 src = './recipes_with_shulkerboxes'
--- shulker_box_generators.py
+++ shulker_box_generators.py
@@ -41,19 +41,16 @@ def generate_filled_shulker_box_nbt(item: str) -> object:
 
     return filled_box
 
-def generate_all_shulker_box_variants(item: str, left_over: bool) -> list:
-    list_of_variants = list()
-    for box in all_shulker_box_types:
-        single_variant = dict()
-        single_variant['item'] = box
-        single_variant['data'] = {
-            'require': generate_filled_shulker_box_nbt(item)
-        }
-        if left_over:
-            single_variant['remainder'] = {'item': box }
-        list_of_variants.append(single_variant)
+def generate_all_shulker_box_variants(item: str, left_over: bool) -> dict:
+    single_variant = dict()
+    single_variant['tag'] = 'grouping:shulkers'
+    single_variant['data'] = {
+        'require': generate_filled_shulker_box_nbt(item)
+    }
+    if left_over:
+        single_variant['remainder'] = {'item': 'minecraft:shulker_box' }
     
-    return list_of_variants
+    return single_variant
 
 def generate_result_shulker_box(item: str):
     return {
